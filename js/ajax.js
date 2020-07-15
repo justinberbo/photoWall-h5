58e3ac85f209b0cf8965985f16581919780747b2
zhuangzihao <tmdsbtjjtds@foxmail.com>
2020-07-15 17:50:15 +0800

    初始化

---
+++ js/ajax.js
@@ -0,0 +1,43 @@
+
+
+function register1() {
+  var name = $("#login_name").val();
+  var password = $("#password").val();
+  $.ajax({
+    type: "post",
+    url: "http://localhost:8080/register",
+    data: {
+      "name": name,
+      "password": password
+    },
+    contentType: "application/json",
+    dataType : "json",
+    success: function (data) {
+      alert("成功")
+    },
+    error:function () {
+      alert("错误")
+    }
+  });
+}
+
+function ajax() {
+  var name = $("#login_name").val();
+  var password = $("#password").val();
+  alert(name+password)
+  $.ajax({
+    type: "post",
+    url: "http://localhost:8080/register",
+    data: {
+      "name": name,
+      "password": password
+    },
+    dataType : "json",
+    success: function (data) {
+      alert("成功")
+    },
+    error:function () {
+      alert("错误")
+    }
+  });
+}
