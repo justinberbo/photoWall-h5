8cf46bf604706636f9b918f5829d7082b104108d
zhuangzihao <tmdsbtjjtds@foxmail.com>
2020-07-15 19:02:55 +0800

    修改脚本

--- bulid.sh
+++ bulid.sh
@@ -1,6 +1,6 @@
 # 解压成war包
-jar -cvfM0 photoWall.war ./photoWall-h5/
+jar -cvfM0 ./
 # 复制到tomcat目录
-cp -f ./photoWall-h5/photoWall.war  /usr/local/apache-tomcat-9.0.37/webapps
+cp -f photoWall.war  /usr/local/apache-tomcat-9.0.37/webapps
 #删除jenkin目录
 #rm -rf photoWall-h5/
